5ed7ae551fb105677b9c1d5ed85e6634df0b7700
Gagi Shmagi <gagi.shmagi@gmail.com>
2023-09-10 21:28:15 +0300

    Changed Browser from google chrome to edge in TestSelenium

--- src/main/java/TestSelenium.java
+++ src/main/java/TestSelenium.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class TestSelenium {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class TestSelenium {
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver","drivers/chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.edge.driver","drivers/msedgedriver.exe");
+        WebDriver driver = new EdgeDriver();
+
+        driver.get("https://www.google.com");
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
 }
